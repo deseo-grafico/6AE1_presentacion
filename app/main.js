9fcc569c61333702473de2e6ee9c73b3d8252473
deseo-grafico <deseo.grafico@gmail.com>
2021-05-27 22:34:29 +0200

    commit inicial

---
+++ app/main.js
@@ -0,0 +1,47 @@
+import '../assets/sass/main.scss'
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+window.addEventListener('load', () => {
+    initEventsInSidebar();
+    initAllSwiper();
+})
+
+const initEventsInSidebar = () => {
+    const sideItems = document.querySelectorAll('.menu_item');
+    sideItems.forEach(i => {
+        i.addEventListener('click', (ev) => {
+            sideItems.forEach(i_ => {
+                if (!ev.composedPath().includes(i)) {
+                    i_.classList.remove('opened');
+                }
+            })
+            if (i.querySelector('.submenu')) {
+                i.classList.toggle('opened');
+            }
+        })
+    })
+}
+
+
+const initAllSwiper = () => {
+    const sectionsSwiper = document.querySelectorAll('.section_swiper');
+    sectionsSwiper.forEach(ss => {
+        const swiperContainer = ss.querySelector('.swiper-container');
+        const swiper = new Swiper(swiperContainer, {
+            direction: 'horizontal',
+            slidesPerView: ss.dataset.slidesPerView ? +ss.dataset.slidesPerView : 1,
+            loop: true,
+            pagination: {
+              el: ss.querySelector('.swiper-pagination'),
+            },
+            navigation: {
+              nextEl: ss.querySelector('.swiper-button-next'),
+              prevEl: ss.querySelector('.swiper-button-prev'),
+            },
+            scrollbar: {
+              el: ss.querySelector('.swiper-scrollbar'),
+            },
+          });
+    })
+}
